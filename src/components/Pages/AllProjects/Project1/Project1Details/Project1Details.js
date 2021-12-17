4d0ac5fc2ac4a7b37a0146e2a49954c6e7751835
Sourov-Paul <sourovpaul583@gmail.com>
2021-12-18 00:52:43 +0600

    check2

--- src/components/Pages/AllProjects/Project1/Project1Details/Project1Details.js
+++ src/components/Pages/AllProjects/Project1/Project1Details/Project1Details.js
@@ -1,107 +1,81 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import "./Project1Details.css";
 import Navegation from './../../../Navegation/Navegation';
 
 
 const Project1Details = () => {
-  const settings = {
-    className: "center",
-    centerMode: true,
-    infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
-    speed: 500,
-  };
+
 
   return (
-
     <div className="main_div ">
       <Navegation></Navegation>
-    <div className="second_div pb-5 mt-5">
-      <div className="container row">
-        <h2 className="text-center mt-3 mb-4 typeDelivery">Project 1</h2>
-        <Slider {...settings}>
-          <div className="col-sm-12 mt-3">
-            <div>
-             
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/HVdnzTK/Screenshot-83.png                "
-                alt=""
-              />
-            </div>
-          </div>
 
-          <div className="col-sm-12 mt-3">
-            <div>
-             
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/GQzs3vb/Screenshot-84.png                "
-                alt=""
-              />
+      <div className="bg-color">
+        <div className="slider d-block">
+          <div className="rotator">
+            <div className="items">
+              <img src="https://i.ibb.co/HVdnzTK/Screenshot-83.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/GQzs3vb/Screenshot-84.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/8XyDpCF/Screenshot-85.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/rmJ6X0K/Screenshot-86.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/VJdHQkD/Screenshot-87.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/6trvqkf/Screenshot-89.png " alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/44LNdqN/Screenshot-88.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/nnmGZNm/Screenshot-127.png" alt="" />
+            </div>
+            <div className="items">
+              <img src="https://i.ibb.co/2sngTWZ/Screenshot-128.png" alt="" />
             </div>
           </div>
-
-          <div className="col-sm-12 mt-3">
-            <div>
-            
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/8XyDpCF/Screenshot-85.png                "
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="col-sm-12 mt-3">
-            <div>
-             
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/rmJ6X0K/Screenshot-86.png                "
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="col-sm-12 mt-3 ">
-            <div>
-             
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/VJdHQkD/Screenshot-87.png                "
-                alt=""
-              />
-            </div>
-          </div>
-          <div className="col-sm-12 mt-3">
-            <div>
-            
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/6trvqkf/Screenshot-89.png                "
-                alt=""
-              />
-            </div>
-          </div>
-          
-          <div className="col-sm-12 mt-3">
-            <div>
-            
-              <img
-                className="img-fluid"
-                src="https://i.ibb.co/44LNdqN/Screenshot-88.png                "
-                alt=""
-              />
-            </div>
-          </div>
-         
-        </Slider>
+        </div>
       </div>
+      <section className="project1-DETAILS ">
+        <div className="row ">
+          <div className="col">
+            <ul className="p1-details">
+              <li>
+                <a target="blank" href="https://happy-shopping-camera.web.app/">
+                  Live Link
+                </a>
+              </li>
+              <li>
+                <a
+                  target="blank"
+                  href="https://github.com/Sourov-Paul/Assignment-12-clint-site"
+                >
+                  Front-Code
+                </a>
+              </li>
+              <li>
+                <a
+                  target="blank"
+                  href="https://github.com/Sourov-Paul/Assignment-12-Server-Site"
+                >
+                  Server-Code
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col"></div>
+        </div>
+      </section>
     </div>
-  </div>
-  )
+  );
   }
 export default Project1Details;
--- src/components/Pages/Banner/Banner.js
+++ src/components/Pages/Banner/Banner.js
@@ -4,6 +4,7 @@ import "./Banner.css";
 import Fade from 'react-reveal/Fade';
 import { Link } from 'react-router-dom';
 import Typewriter from 'typewriter-effect';
+import Particles from "react-tsparticles";
 
 
 
@@ -12,8 +13,104 @@ import Typewriter from 'typewriter-effect';
 
 
 const Banner = () => {
+
+  
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div>
+          <Particles
+          
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "white",
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 0.5,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 3000,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#ffffff",
+          },
+          links: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 4,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+
       <div className="main_banner">
         <Navegation></Navegation> <br /> <br /> <br />
         <div className="second-div">
